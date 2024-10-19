26ef905555b2094685512b8371921a81a8f77720
Carlos Teixeira <c.texeirajr@gmail.com>
2024-10-19 17:03:17 -0300

    configurando timezone
    entendendo melhor como funciona o servidor

---
+++ ex002/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>Exemplo de PHP</h1>
+    <?php date_default_timezone_set("America/Sao_Paulo")?>
+    <?php 
+        $nome = "Carlos";
+        $anoNascimento = 1996;
+        $idade = date("Y") - $anoNascimento;
+    ?>
+    <p><?php echo "Hoje é dia " . date("d/m/Y") . " / " . "Hora atual: " . date("G:i:s");?></p>
+    <p><?php echo "Meu nome é $nome e minha idade é $idade anos"?></p>
+</body>
+</html>
