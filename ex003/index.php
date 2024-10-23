35133f33d962ac91beb77931d8adcea7e144b48a
Carlos Teixeira <c.texeirajr@gmail.com>
2024-10-23 11:06:25 -0300

    Tipos Primitivos em PHP
    + tipos primitivos

---
+++ ex003/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos Primitivos em PHP</title>
+</head>
+<body>
+    <h1>Teste de Tipos Primitivos</h1>
+    <?php
+        // 0x = hexadecimal 0b = binário 0 = octal 
+        // $num = 300; | $num = 0x1A; $num = 0b1011;
+        $num = 010;
+        echo "O valor da variável é $num";
+    ?>
+</body>
+</html>
