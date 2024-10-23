e2a56053836fc8756c816afed260ddc08ec0ba1b
Carlos Teixeira <c.texeirajr@gmail.com>
2024-10-23 11:21:13 -0300

    Tipos Primitivos em PHP / var_dump()
    + mostrar tipo primitivo da variável com var_dump()

--- ex003/index.php
+++ ex003/index.php
@@ -10,8 +10,12 @@
     <?php
         // 0x = hexadecimal 0b = binário 0 = octal 
         // $num = 300; | $num = 0x1A; $num = 0b1011;
-        $num = 010;
-        echo "O valor da variável é $num";
+        // $num = 010;
+        // echo "O valor da variável é $num";
+
+        $v = "Carlos";
+        var_dump($v);
+
     ?>
 </body>
 </html>
