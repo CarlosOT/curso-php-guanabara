55d45c3d1b25a8788f39ed4c6c67b5f5905b0490
Carlos Teixeira <c.texeirajr@gmail.com>
2024-10-29 15:10:04 -0300

    Interação com Formulários parte 2
    +operador de coalescência nula

--- ex004/cad.php
+++ ex004/cad.php
@@ -12,8 +12,13 @@
     </header>
     <main>
         <?php 
-            var_dump($_GET);
+            // var_dump($_GET); // $_REQUEST FUNCIONA COM O MÉDOTO GET E POST, É A JUNÇÃO DE $_GET, $_POST E $_COOKIES
+            $nome = $_GET["nome"] ?? "sem nome"; //operador de coalescência nula
+            $sobrenome = $_GET["sobrenome"] ?? "desconhecido"; //operador de coalescência nula
+
+            echo "<p>Parabéns <strong>$nome $sobrenome</strong> por estar aprendendo PHP</p>";
         ?>
+        <p><a href="javascript:history.go(-1)">Voltar para a página anterior</a></p>
     </main>
 </body>
 </html>
